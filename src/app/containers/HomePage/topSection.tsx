548f8d1a443177f83a1122d7120f85377d66b558
ElioRivas <eliocarmen@hotmail.com>
2022-10-17 09:35:48 -0600

    Buttons added

--- src/app/containers/HomePage/topSection.tsx
+++ src/app/containers/HomePage/topSection.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import styled from 'styled-components'
 import tw from 'twin.macro'
 
-import MclarenCarImg from "../../../assets/images/mclaren-orange-big.png";
-import BlobImg from "../../../assets/images/blob.svg";
-import { SCREENS } from '../../components/responsive';
+import MclarenCarImg from '../../../assets/images/mclaren-orange-big.png'
+import BlobImg from '../../../assets/images/blob.svg'
+import { SCREENS } from '../../components/responsive'
+import { Button } from '../../components/button/index'
+
 
 const TopSectionContainer = styled.div`
   min-height: 400px;
@@ -19,7 +21,7 @@ const TopSectionContainer = styled.div`
         lg:pl-12
         lg:pr-12
     `};
-`;
+`
 
 const LeftContainer = styled.div`
   ${tw`
@@ -27,7 +29,7 @@ const LeftContainer = styled.div`
         flex
         flex-col    
     `};
-`;
+`
 
 const RightContainer = styled.div`
   ${tw`
@@ -37,7 +39,7 @@ const RightContainer = styled.div`
         relative  
         mt-20 
     `};
-`;
+`
 
 const Slogan = styled.h1`
   ${tw`
@@ -54,7 +56,7 @@ const Slogan = styled.h1`
     lg: leading-normal
     xl: leading-relaxed
 `};
-`;
+`
 
 const Description = styled.p`
   ${tw`
@@ -63,100 +65,109 @@ const Description = styled.p`
     xl: text-lg
     sm: max-h-full
     overflow-hidden
-    max-h-14
+    max-h-20
     text-gray-800
 `};
-`;
+`
 
 const BlobContainer = styled.div`
-    width: 20em;
-    height: 10em;
-    position: absolute;
-    right: -5em;
-    top: -9em;
-    
-    z-index: -1;
-    transform: rotate(-30deg);
+  width: 20em;
+  height: 10em;
+  position: absolute;
+  right: -5em;
+  top: -9em;
 
-    img {
-        width: 100%;
-        height: auto;
-        max-height: max-content;
-    }
-    @media(min-width: ${SCREENS.sm} ){
-        width: 40em;
-        max-height: 10em;
-        right: -9em;
-        top: -16em;
-        transform: rotate(-25deg);
-    }
-    @media(min-width: ${SCREENS.lg} ){
-        width: 50em;
-        max-height: 30em;
-        right: -7em;
-        top: -15em;
-        transform: rotate(-30deg);
-    }
-    @media(min-width: ${SCREENS.xl} ){
-        width: 70em;
-        max-height: 30em;
-        right: -15em;
-        top: -25em;
-        transform: rotate(-20deg);
-    }
-`;
+  z-index: -1;
+  transform: rotate(-30deg);
+
+  img {
+    width: 100%;
+    height: auto;
+    max-height: max-content;
+  }
+  @media (min-width: ${SCREENS.sm}) {
+    width: 40em;
+    max-height: 10em;
+    right: -9em;
+    top: -16em;
+    transform: rotate(-25deg);
+  }
+  @media (min-width: ${SCREENS.lg}) {
+    width: 50em;
+    max-height: 30em;
+    right: -7em;
+    top: -15em;
+    transform: rotate(-30deg);
+  }
+  @media (min-width: ${SCREENS.xl}) {
+    width: 70em;
+    max-height: 30em;
+    right: -15em;
+    top: -25em;
+    transform: rotate(-20deg);
+  }
+`
 
 const StandaloneCar = styled.div`
-    width: auto;
-    height: 10em;
-    right: -6em;
-    top: -5em;
-    position: absolute;
+  width: auto;
+  height: 10em;
+  right: -6em;
+  top: -5em;
+  position: absolute;
 
-    img{
-        width:auto;
-        height:100%;
-        max-width: fit-content;
-    }
-    @media(min-width: ${SCREENS.sm} ){
-        height: 16em;
-        right: -6em;
-        top: -6em;
-    }
-    @media(min-width: ${SCREENS.lg} ){
-        height: 21em;
-        right: -8em;
-        top: -5em;
-    }
-    @media(min-width: ${SCREENS.xl} ){
-        height: 30em;
-        right: -13em;
-        top: -9em;
-    }
+  img {
+    width: auto;
+    height: 100%;
+    max-width: fit-content;
+  }
+  @media (min-width: ${SCREENS.sm}) {
+    height: 16em;
+    right: -6em;
+    top: -6em;
+  }
+  @media (min-width: ${SCREENS.lg}) {
+    height: 21em;
+    right: -8em;
+    top: -5em;
+  }
+  @media (min-width: ${SCREENS.xl}) {
+    height: 30em;
+    right: -13em;
+    top: -9em;
+  }
+`
+
+const ButtonsContainer = styled.div`
+  ${tw`
+    flex
+  `};
 `;
 
-
 export function TopSection() {
-
-    return (
-        <TopSectionContainer>
-            <LeftContainer>
-                <Slogan>Buy the Best Quality Car's with Us</Slogan>
-                <Description>
-                    Always choose the best car from our local stores or order it remotely
-                    at the best price for you and get the best quality cars for as long as you like
-                </Description>
-            </LeftContainer>
-            <RightContainer>
-                <BlobContainer>
-                    <img src={BlobImg} />
-                </BlobContainer>
-                <StandaloneCar>
-                    <img src={MclarenCarImg} />
-                </StandaloneCar>
-            </RightContainer>
-        </TopSectionContainer>
-    );
+  return (
+    <TopSectionContainer>
+      <LeftContainer>
+        <Slogan>Buy the Best Quality Car's with Us</Slogan>
+        <Description>
+          Always choose the best car from our local stores or order it remotely
+          at the best price for you and get the best quality cars for as long as
+          you like
+        </Description>
+        <ButtonsContainer>
+          <Button text='Book Your Ride'/>
+          <Button theme='filled' text='Sell Your Car'/>
+        </ButtonsContainer>
+      </LeftContainer>
+      <RightContainer>
+        <BlobContainer>
+          <img src={BlobImg} />
+        </BlobContainer>
+        <StandaloneCar>
+          <img src={MclarenCarImg} />
+        </StandaloneCar>
+      </RightContainer>
+    </TopSectionContainer>
+  )
 }
 
 //1:30:12 / 8:15:43 Create landing page Cars top section
