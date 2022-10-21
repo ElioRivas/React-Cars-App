1b6420d48eb9598bde389e574f4448f01f560c20
ElioRivas <eliocarmen@hotmail.com>
2022-10-20 19:18:42 -0600

    Minor Fixes to Footer

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -1,17 +1,50 @@
+import { faEnvelope, faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import styled from 'styled-components';
 import tw from 'twin.macro';
 import { Logo } from '../logo';
 
 const FooterContainer = styled.div`
-    min-height: 30em;
-    background-color: #1d2124;    
+    //min-height: 24em;
+    background-color: #1d2124;
     ${tw`
-    flex
-    flex-wrap    
-    min-w-full        
-    pt-7
-    pb-1
+        flex
+        flex-col        
+        min-w-full
+        pt-10
+        md:pt-16        
+        items-center
+        justify-center        
+    `};
+`;
+
+const InnerContainer = styled.div`
+    ${tw`
+        flex
+        w-full
+        h-full
+        max-w-screen-2xl
+        flex-wrap      
+    `};
+`;
+
+const BottomContainer = styled.div`
+    ${tw`
+        w-full
+        flex
+        max-w-screen-2xl
+        justify-center
+        md:justify-start
+        mt-7
+        md:mt-1
+    `};
+`;
+
+const CopyRightText = styled.small`
+    font-size:12px;
+    ${tw`
+        text-gray-300
     `};
 `;
 
@@ -19,16 +52,179 @@ const AboutContainer = styled.div`
     ${tw`
     flex
     flex-col
+    mr-2
+    md:mr-16
+    pl-10
+    pr-10
+    md:pl-3
+    md:pr-3
+    `};
+`; 
+
+const AboutText = styled.p`
+    ${tw`
+        text-white
+        text-sm
+        font-normal
+        max-w-xs
+        leading-5
+        mt-2
+    `};
+`;
+
+const SectionContainer = styled.div`
+    ${tw`
+        w-full
+        md:w-auto
+        flex 
+        flex-col        
+        mr-2
+        md:mr-16
+        pl-10
+        pr-10
+        md:pl-3
+        md:pr-3
+        mt-7
+        md:mt-0
+    `};
+`;
+
+const LinksList = styled.ul`
+    ${tw `
+        outline-none
+        list-none
+        flex-col
+
+    `};
+`;
+
+const ListItem = styled.li`
+    ${tw`    
+    mb-3
+    `};
+
+    & > a {
+        ${tw`
+            text-sm
+            text-white
+            transition-all
+            hover: text-gray-200
+        `};
+    }
+`;
+
+const HeaderTitle =styled.h3`
+    ${tw`
+        text-2xl
+        font-bold
+        text-white
+        mb-3
+    `};
+`;
+
+const HorizontalContainer = styled.div`
+    ${tw`
+        flex       
+        items-center 
+    `};
+`;
+
+const ReadIcon = styled.span`
+    ${tw`
+        w-10
+        h-10
+        rounded-full
+        bg-red-500
+        flex
+        items-center
+        justify-center
+        text-white
+        text-base
+        mr-2
+    `};
+`;
+
+const SmallText = styled.h6`
+    ${tw`
+        text-sm
+        text-white
     `};
 `;
 
 export function Footer(){
     return (
         <FooterContainer>
+            <InnerContainer>
             <AboutContainer>
-                <Logo color='white' bgColor='dark'/>
+                <Logo color="white" bgColor="dark" />
+                <AboutText>
+                Your Car is a Car Renting and Selling company located in many
+                countries across the world which has high quality cars and top
+                rated service.
+                </AboutText>
             </AboutContainer>
+            <SectionContainer>
+                <HeaderTitle>Our Links</HeaderTitle>
+                <LinksList>
+                <ListItem>
+                    <a href="#">Home</a>
+                </ListItem>
+                <ListItem>
+                    <a href="#">About Us</a>
+                </ListItem>
+                <ListItem>
+                    <a href="#">Services</a>
+                </ListItem>
+                <ListItem>
+                    <a href="#">Models</a>
+                </ListItem>
+                <ListItem>
+                    <a href="#">Blog</a>
+                </ListItem>
+                </LinksList>
+            </SectionContainer>
+            <SectionContainer>
+                <HeaderTitle>Other Links</HeaderTitle>
+                <LinksList>
+                <ListItem>
+                    <a href="#">FAQ</a>
+                </ListItem>
+                <ListItem>
+                    <a href="#">Contact us</a>
+                </ListItem>
+                <ListItem>
+                    <a href="#">Support</a>
+                </ListItem>
+                <ListItem>
+                    <a href="#">Privacy Policy</a>
+                </ListItem>
+                <ListItem>
+                    <a href="#">Terms &amp; Conditions </a>
+                </ListItem>
+                </LinksList>
+            </SectionContainer>
+            <SectionContainer>
+                <HeaderTitle>Call Now</HeaderTitle>
+                <HorizontalContainer>
+                    <ReadIcon>
+                        <FontAwesomeIcon icon={faPhoneAlt}/>
+                    </ReadIcon>
+                    <SmallText>+91 555-234-8564</SmallText>
+                </HorizontalContainer>
+            </SectionContainer> 
+            <SectionContainer>
+                <HeaderTitle>Mail</HeaderTitle>
+                <HorizontalContainer>
+                    <ReadIcon>
+                        <FontAwesomeIcon icon={faEnvelope }/>
+                    </ReadIcon>
+                    <SmallText>info@yourcar.com</SmallText>
+                </HorizontalContainer>
+            </SectionContainer>              
+            </InnerContainer>
+            <BottomContainer>
+                <CopyRightText>Copyright &copy; {new Date().getFullYear()} YourCar. All rights reserved.</CopyRightText>
+            </BottomContainer>
         </FooterContainer>
-
     );
 }
